6e447aea56f1390ccf4bd2d2c1305cf5792c5006
amanbro123922 <146796475+amanbro123922@users.noreply.github.com>
2023-10-03 09:09:37 +0530

    all files second resume

---
+++ Home Page/script.js
@@ -0,0 +1,22 @@
+
+// Loader Animation 
+let loaderBody = document.querySelector("#loader");
+ 
+ window.addEventListener("load", function() {
+     loaderBody.style.visibility = "hidden";
+})
+
+
+// Sidebar Menu 
+ let sidebarbody = document.getElementById('side-bar-body');
+
+ function openSidebar() {
+
+  sidebarbody.classList.toggle('visibleSidebar')
+ }
+
+
+
+
+
+ 
